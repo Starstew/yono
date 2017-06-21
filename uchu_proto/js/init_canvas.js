02028889dd06d3f9d7737f967304c25cb3341759
Ed Stastny <dandystarstew@gmail.com>
2017-06-21 12:32:56 -0700

    Parameterized init

--- uchu_proto/js/init_canvas.js
+++ uchu_proto/js/init_canvas.js
@@ -2,7 +2,6 @@
 $(function(){
 	yono.data.init({"jsonData":uchuDataJson}); // for local environment
 	// yono.data.init({"jsonPath":"uchuData.json"}); // for server environment
-	yono.canvas.init();
 });
 yono.data.jsonDataLoadComplete = function() {
 	var pst = yono.data.getPiecesSortedBySubtime();
@@ -10,6 +9,7 @@ yono.data.jsonDataLoadComplete = function() {
 	for (var i=0;i<pst.length;i++) {
 		img_arr.push(pst[i]);
 	}
+	yono.canvas.init({"pieces_set":"2X"});
 	yono.canvas.loadImages(img_arr);
 	setTimeout(function(){
 		var params = {yid:"045_OED",depth:2,speed:1000};
